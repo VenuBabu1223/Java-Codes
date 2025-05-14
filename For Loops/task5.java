a93d7bc29150526bde17b3b4caa606aecc66e1d1
VenuBabu1223 <124777378+VenuBabu1223@users.noreply.github.com>
2025-05-14 12:30:34 +0530

    Task on for loops

---
+++ For Loops/task5.java
@@ -0,0 +1,8 @@
+public class task5 {
+    public static void main(String[] args) {
+        System.out.println("Numbers increasing by 5:");
+        for(int i=5;i<=50;i=i+5){
+            System.out.print(i+" ");
+        }
+    }
+}
